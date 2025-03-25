b1e70b9de4795dde26d647cc671d668df351c936
Zonkkatul <mervezonkk@gmail.com>
2025-03-25 11:10:31 +0100

    added swagger

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -6,48 +8,204 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Server is running in http://localhost:${PORT}`);
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Users API",
+      version: "1.0.0",
+      description: "A simple Express users API",
+    },
+  },
+  apis: ["./server.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
 });
 
 let users = [
-    {
-        id: 1, name: "1984", surname: "kjslkdjfksld"
-    },
-    {
-        id: 2, name: "gdfgdfg", surname: "gdfgf"
-
-    },
+  {
+    id: 1,
+    name: "1984",
+    surname: "kjslkdjfksld",
+  },
+  {
+    id: 2,
+    name: "gdfgdfg",
+    surname: "gdfgf",
+  },
 ];
 
-app.get("/users", (req,res) => {
-    res.json(users);
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieves all users.
+ *     description: Retrieve a list of users.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   surname:
+ *                     type: string
+ */
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
-app.post("/users", (req,res) => {
-const newUser = {
-    id: users.length +1,
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Add a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - surname
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User added succesfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   surname:
+ *                     type: string
+ */
+
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
     name: req.body.name,
     surname: req.body.surname,
-};
-users.push(newUser);
-res.json({message: "New user added succesfully!", user: newUser})
+  };
+  users.push(newUser);
+  res.json({ message: "New user added succesfully!", user: newUser });
 });
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update an existing user.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric ID of the user to update.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               surname:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User updated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     name:
+ *                       type: string
+ *                     surname:
+ *                       type: string
+ *       404:
+ *         description: User not found.
+ */
 
 
 app.put("/users/:id", (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find((b) => b.id === userId);
-    if(!user) {
-        return res.status(404).json({message: "user not found"});
-    }
-    user.name = req.body.name || user.name;
-    user.surname = req.body.surname || user.surname;
-    res.json({message: "user updated succesfully", user});
+  const userId = parseInt(req.params.id);
+  const user = users.find((b) => b.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  user.name = req.body.name || user.name;
+  user.surname = req.body.surname || user.surname;
+  res.json({ message: "user updated succesfully", user });
 });
 
-app.delete("/users/:id", (req,res) => {
-    const userId = parseInt(req.params.id);
-    users = users.filter((b) => b.id !== userId);
-    res.json({message: "User deleted succesfully!"});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric ID of the user to delete.
+ *     responses:
+ *       200:
+ *         description: User deleted successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       404:
+ *         description: User not found.
+ */
+
+
+
+
+app.delete("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  users = users.filter((b) => b.id !== userId);
+  res.json({ message: "User deleted succesfully!" });
 });
